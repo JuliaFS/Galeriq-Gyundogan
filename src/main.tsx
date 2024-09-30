5cc03453c24493418058fdb4187d3d33abe9cab6
Yulia <yuliya.f.s@gmail.com>
2024-09-30 08:50:45 +0300

    change path for home

--- src/main.tsx
+++ src/main.tsx
@@ -7,13 +7,13 @@ import { Provider } from 'react-redux'
 import { store } from './store/index.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+ // <React.StrictMode>
     <Provider store={store}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
     </Provider>
-  </React.StrictMode>,
+ // </React.StrictMode>,
 )
 
 
